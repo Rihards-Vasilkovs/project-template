1c8871e8e5427dbe52c4085b9a5f2d2e5ad92962
Rihards <eaa23riva@students.eaaa.dk>
2024-05-27 11:19:04 +0200

    created grid of teachers

--- app.js
+++ app.js
@@ -3,6 +3,31 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const teachers = await getTeachers();
+  console.log(teachers);
+  displayTeachers(teachers);
+  displayTeachersGrid(teachers);
+}
+
+async function getTeachers() {
+}
+
+function displayTeachersGrid(teachers) {
+  const teachersGrid = document.querySelector("#teachers-grid");
+
+  for (const teacher of teachers) {
+	  teachersGrid.insertAdjacentHTML(
+      "beforeend",
+    `
+    <article class="grid-item">
+    <img src="${teacher.image}" alt="${teacher.name}" />
+    <h2>${teacher.name}</h2>
+    <p>${teacher.title}</p>
+    <a>${teacher.mail}</a>
+    </article>
+    `
+    );
+  }
 }
